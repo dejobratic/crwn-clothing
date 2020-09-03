2524c89e45ab53f1f128e5f10ae831576703e879
dejobratic <dejobratic@gmail.com>
2020-09-03 20:18:01 +0200

    Added initial homepage layout.

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -2,8 +2,13 @@ import React from "react"
 
 import "app/App.css"
 
+import HomePage from "app/views/HomePage"
+
 function App() {
-  return <div>Hello, world!</div>
+  return (<div>
+    <h1>Hello, world!</h1>
+    <HomePage></HomePage>
+  </div>)
 }
 
 export default App
